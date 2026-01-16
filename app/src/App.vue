3063ff7302f7dd1a50d740ff6ad2694daf540010
Mathieu DOUHET <mdouhet@mac.home>
2026-01-16 14:59:39 +0100

    Install vue project with pinia wrong version and github actions

---
+++ app/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container mx-auto py-20">
+    <h1>You did it!</h1>
+    <fwb-alert type="success">
+      Success! You can now use Flowbite Vue in your Vue application 🎉
+    </fwb-alert>
+    <p class="text-2xl">
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { FwbAlert } from 'flowbite-vue'
+</script>
--- src/index.js
+++
@@ -1,1 +0,0 @@
-console.log("Projet GitHub Actions minimal");
